dfcfe902563e7169ddf2c41111f3e9736cf2b9ed
SanjayMaity15 <sanjaymaityofficial15@gmail.com>
2026-04-03 08:54:46 +0530

    profile drop down animation add

--- frontend/src/components/auth/ProfileDropDown.jsx
+++ frontend/src/components/auth/ProfileDropDown.jsx
@@ -1,28 +1,43 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 import { VscDashboard, VscRequestChanges, VscSignOut } from "react-icons/vsc";
-import {  useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 
 import useOnClickOutside from "../../hooks/useOnClickOutside";
 
 import Logout from "./Logout";
-
+import { gsap } from "gsap";
 
 export default function ProfileDropdown() {
 	const { user } = useSelector((state) => state.profile);
 	const [open, setOpen] = useState(false);
 	const ref = useRef(null);
-	
-	const navigate = useNavigate()
-	const [openLogoutPopup, setOpenLogoutPopup] = useState(false)
+
+	const navigate = useNavigate();
+	const [openLogoutPopup, setOpenLogoutPopup] = useState(false);
 	useOnClickOutside(ref, () => setOpen(false));
 
-	// handle logout
+	const dropDownRef = useRef(null);
 
+	useEffect(() => {
+		if (!open || !dropDownRef.current) return;
 
+		gsap.fromTo(
+			dropDownRef.current,
+			{
+				x: 100,
+				opacity: 0,
+			},
+			{
+				x: 0,
+				opacity: 1,
+				duration: 0.4,
+				ease: "power2.out",
+			},
+		);
+	}, [open]);
 
-	
 	if (!user) return null;
 
 	return (
@@ -37,13 +52,14 @@ export default function ProfileDropdown() {
 					alt={`profile-${user.firstName}`}
 					className="aspect-square w-10 rounded-full object-cover "
 				/>
-
 			</button>
 
 			{/* Dropdown */}
 			{open && (
-				<div className="absolute top-[150%] -right-2 z-10000">
-		
+				<div
+					className="absolute top-[150%] -right-2 z-10000"
+					ref={dropDownRef}
+				>
 					{/* Menu */}
 					<div className="overflow-hidden rounded-sm border border-primary bg-white transition">
 						<Link
@@ -58,7 +74,7 @@ export default function ProfileDropdown() {
 
 						<div
 							onClick={() => {
-								setOpen(false), setOpenLogoutPopup(true);
+								(setOpen(false), setOpenLogoutPopup(true));
 							}}
 							className="flex cursor-pointer items-center gap-x-2 px-4 py-2.5 text-sm  hover:bg-pink-200"
 						>
@@ -67,7 +83,7 @@ export default function ProfileDropdown() {
 						</div>
 						<div
 							onClick={() => {
-								setOpen(false), navigate("/change-password");
+								(setOpen(false), navigate("/change-password"));
 							}}
 							className="flex cursor-pointer items-center gap-x-2 px-4 py-2.5 text-sm hover:bg-pink-200"
 						>
@@ -80,9 +96,9 @@ export default function ProfileDropdown() {
 
 			{/* logout popup */}
 
-			{
-				openLogoutPopup && <Logout onClose={() => setOpenLogoutPopup(false)}/>
-			}
+			{openLogoutPopup && (
+				<Logout onClose={() => setOpenLogoutPopup(false)} />
+			)}
 		</div>
 	);
 }
